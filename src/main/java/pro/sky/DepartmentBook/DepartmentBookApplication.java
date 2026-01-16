eee02fcba1db45fe25787d7024ed4fe6dda0fdb3
banbezv <banbezv@mail.ru>
2026-01-16 21:00:48 +0300

    Spring Init

---
+++ src/main/java/pro/sky/DepartmentBook/DepartmentBookApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.DepartmentBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepartmentBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepartmentBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/DepartmentBook/DepartmentBookApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.DepartmentBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
