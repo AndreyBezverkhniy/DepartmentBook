e5eebc4dd986292a6a7e8a107060bd2083b1cb28
banbezv <banbezv@mail.ru>
2026-01-29 17:33:44 +0300

    Tests: 1-5

--- src/test/java/pro/sky/DepartmentBook/service/EmployeeServiceTest.java
+++ src/test/java/pro/sky/DepartmentBook/service/EmployeeServiceTest.java
@@ -1,6 +1,7 @@
 package pro.sky.DepartmentBook.service;
 
 import org.junit.jupiter.api.Test;
+import pro.sky.DepartmentBook.exception.EmployeePresenceException;
 import pro.sky.DepartmentBook.model.Employee;
 
 import java.util.List;
@@ -16,7 +17,64 @@ class EmployeeServiceTest {
         assertDoesNotThrow(() -> {
             employeeService.add("123", 1, 100);
         });
+        List<Employee> expected = List.of(new Employee(1, "123", 1, 100));
+        List<Employee> actual = employeeService.getAll();
+        assertIterableEquals(expected, actual);
+    }
+
+    @Test
+    public void givenOneEmployeeWhenAddUnaddedEmployeeThenGetAllReturnsBoth() {
+        employeeService = new EmployeeServiceImpl();
+        assertDoesNotThrow(() -> {
+            employeeService.add("123", 1, 100);
+        });
+        assertDoesNotThrow(() -> {
+            employeeService.add("456", 2, 200);
+        });
+        List<Employee> expected = List.of(new Employee(1, "123", 1, 100), new Employee(1, "456", 2, 200));
+        List<Employee> actual = employeeService.getAll();
+        assertIterableEquals(expected, actual);
+    }
+
+    @Test
+    public void givenOneEmployeeWhenAddWithSameFioThenGetAllReturnsOldOne() {
+        employeeService = new EmployeeServiceImpl();
+        assertDoesNotThrow(()->{
+            employeeService.add("123",1,100);
+        });
+        try{
+            employeeService.add("123",2,200);
+        } catch(RuntimeException e){}
         List<Employee> expected=List.of(new Employee(1,"123",1,100));
+        List<Employee> actual=employeeService.getAll();
+        assertIterableEquals(expected,actual);
+    }
+
+    @Test
+    public void givenOneEmployeeWhenAddWithSameFioThenThrows(){
+        employeeService=new EmployeeServiceImpl();
+        assertDoesNotThrow(()->{
+            employeeService.add("123",1,100);
+        });
+        assertThrows(EmployeePresenceException.class,()->{
+            employeeService.add("123",2,200);
+        });
+    }
+
+    @Test
+    public void givenRemoveAddedOneEmployeeWhenAddWithSameFioThenGetAllReturnsOne(){
+        employeeService=new EmployeeServiceImpl();
+        assertDoesNotThrow(()->{
+            Employee employee=employeeService.add("123",1,100);
+        });
+        assertDoesNotThrow(()->{
+            int id=employeeService.getAll().get(0).getId();
+            employeeService.remove(id);
+        });
+        assertDoesNotThrow(()->{
+            employeeService.add("123",2,200);
+        });
+        List<Employee> expected=List.of(new Employee(1,"123",2,200));
         List<Employee> actual=employeeService.getAll();
         assertIterableEquals(expected,actual);
     }
